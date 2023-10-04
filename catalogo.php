e57696a891bf9d658903ccca3ad71463c7fca97e
ivn16xg <146996444+ivn16xg@users.noreply.github.com>
2023-10-04 14:02:33 -0600

    Add files via upload

---
+++ catalogo.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="recursos/plugins/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+
+	<title>Jr</title>
+</head>
+<body>
+	<?php
+	require_once "header.php";
+	?>
+	<br><br><br><br><br>
+		
+	<div class="container" align="center">
+		<h1>Echa un vistazo</h1>	
+	<hr>
+	</div>
+	
+
+
+	<div class="container">
+			<div class="row" align="center">
+<!-- 559x960-->
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva1.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">Sofía </font>
+						<br>
+					27 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva2.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">Alexandra </font>
+						<br>
+					24 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva3.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva4.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva5.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva6.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva7.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva8.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva9.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva10.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<img src="recursos/img/Diapositiva11.PNG" class="imag" style="border-radius: 15px; width: 250px; height: 421px;">
+					<b><font color="red" size="5"><p class="mt-1">María </font>
+						<br>
+					25 años</p></b>
+				</div>
+
+				<div class="col-xs-12 col-sm-6 col-md-6 col-lg-4 col-xl-4">
+					<div class="video-container">
+				        <video class="imag" controls style="border-radius: 15px; width: 250px; height: 421px;">
+				            <source src="recursos/img/video.mp4" type="video/mp4">
+				            Tu navegador no admite la reproducción de video.
+				        </video>
+					        <b><font color="red" size="5"><p>María </font>
+								<br>
+							25 años</p></b>
+				    </div>
+			    </div>
+
+  			</div>
+	</div>
+	
+
+
+	<br><br>
+
+
+	<script src="recursos/plugins/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
